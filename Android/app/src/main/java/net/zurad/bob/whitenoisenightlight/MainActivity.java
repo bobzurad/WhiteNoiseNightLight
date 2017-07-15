490a8b294854c046fd162f365307505b8fb9c627
Bob Zurad <bob@zurad.net>
2017-07-15 17:27:05 -0500

    created slider

--- Android/app/src/main/java/net/zurad/bob/whitenoisenightlight/MainActivity.java
+++ Android/app/src/main/java/net/zurad/bob/whitenoisenightlight/MainActivity.java
@@ -1,13 +1,34 @@
 package net.zurad.bob.whitenoisenightlight;
 
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout _mainLayout;
+    SeekBar _seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _mainLayout = (ConstraintLayout) findViewById(R.id.mainLayout);
+
+        _seekBar = (SeekBar) findViewById(R.id.seekBar);
+        _seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                String hex = Integer.toHexString(progress);
+                _mainLayout.setBackgroundColor(Color.parseColor("#" + hex + hex + hex));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
     }
 }
