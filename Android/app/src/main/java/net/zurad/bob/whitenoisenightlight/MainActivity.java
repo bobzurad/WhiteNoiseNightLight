916d9f87c75e1f97e27c167f08358f10c569eea4
Bob Zurad <bob@zurad.net>
2017-07-20 22:47:42 -0500

    set screen brightness

--- Android/app/src/main/java/net/zurad/bob/whitenoisenightlight/MainActivity.java
+++ Android/app/src/main/java/net/zurad/bob/whitenoisenightlight/MainActivity.java
@@ -1,8 +1,11 @@
 package net.zurad.bob.whitenoisenightlight;
 
+import android.content.ContentResolver;
 import android.graphics.Color;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 
@@ -27,11 +30,28 @@ public class MainActivity extends AppCompatActivity {
                     hex = "0" + hex;
                 }
                 _mainLayout.setBackgroundColor(Color.parseColor("#" + hex + hex + hex));
+                setBrightness(progress);
             }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) { }
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) { }
         });
+    }
+
+    public void setBrightness(int brightness){
+        //constrain the value of brightness
+        if(brightness < 0)
+            brightness = 0;
+        else if(brightness > 255)
+            brightness = 255;
+
+        try {
+            ContentResolver cResolver = this.getApplicationContext().getContentResolver();
+            Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, brightness);
+        } catch (Exception e) {
+            Log.e("Error", "Cannot access screen brightness");
+            e.printStackTrace();
+        }
     }
 }
