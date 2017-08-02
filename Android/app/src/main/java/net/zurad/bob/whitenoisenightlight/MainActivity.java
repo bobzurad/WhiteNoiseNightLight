c7d72314054e3ea3c6c35264c850d6e3afaefb79
Bob Zurad <bob@zurad.net>
2017-08-02 16:10:25 -0500

    added white noise

--- Android/app/src/main/java/net/zurad/bob/whitenoisenightlight/MainActivity.java
+++ Android/app/src/main/java/net/zurad/bob/whitenoisenightlight/MainActivity.java
@@ -1,9 +1,12 @@
 package net.zurad.bob.whitenoisenightlight;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
@@ -13,13 +16,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.WindowManager;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
+import android.widget.Switch;
+import android.media.SoundPool;
 
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout _mainLayout;
     SeekBar _seekBar;
+    Switch _whiteNoiseSwitch;
+    SoundPool _soundPool;
+    int _soundId;
+    int _playId;
+
     boolean _canChangeScreenBrightness;
     int _brightnessOnAppStart;
     int _brightnessModeOnAppStart;
@@ -33,6 +44,10 @@ public class MainActivity extends AppCompatActivity {
 
         _mainLayout = (LinearLayout) findViewById(R.id.mainLayout);
         _seekBar = (SeekBar) findViewById(R.id.seekBar);
+        _whiteNoiseSwitch = (Switch) findViewById(R.id.whiteNoiseSwitch);
+        _soundPool = createSoundPool();
+        _soundId = _soundPool.load(this, R.raw.whitenoise, 1);
+        _soundPool.setLoop(_soundId, -1);
 
         //save starting brightness values
         try {
@@ -64,7 +79,6 @@ public class MainActivity extends AppCompatActivity {
             }
         } else {
             setBrightness(_seekBar.getProgress());
-            Log.i("SeekBar value", Integer.toString(_seekBar.getProgress()));
         }
 
         //logic to handle seekbar
@@ -81,10 +95,30 @@ public class MainActivity extends AppCompatActivity {
                     setBrightness(progress);
                 }
             }
+
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) { }
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) { }
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        //logic for white noise switch
+        _whiteNoiseSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isOn) {
+                if (isOn) {
+                    if (_playId > 0) {
+                        _soundPool.resume(_playId);
+                    } else {
+                        _playId = _soundPool.play(_soundId, 1.0f, 1.0f, 1, -1, 1.0f);
+                    }
+                } else {
+                    _soundPool.pause(_playId);
+                }
+            }
         });
     }
 
@@ -119,7 +153,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M
-                && requestCode == CODE_WRITE_SETTINGS_PERMISSION && Settings.System.canWrite(this)){
+                && requestCode == CODE_WRITE_SETTINGS_PERMISSION && Settings.System.canWrite(this)) {
             _canChangeScreenBrightness = true;
             setBrightness(getResources().getInteger(R.integer.defaultBrightness));
         }
@@ -134,11 +168,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void setBrightness(int brightness){
+    public void setBrightness(int brightness) {
         //constrain the value of brightness
-        if(brightness < 0)
+        if (brightness < 0)
             brightness = 0;
-        else if(brightness > getResources().getInteger(R.integer.brightnessMax))
+        else if (brightness > getResources().getInteger(R.integer.brightnessMax))
             brightness = getResources().getInteger(R.integer.brightnessMax);
 
         try {
@@ -155,5 +189,30 @@ public class MainActivity extends AppCompatActivity {
             Log.e("Error", "Cannot access screen brightness");
             e.printStackTrace();
         }
+    }
+
+    protected SoundPool createSoundPool() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return createNewSoundPool();
+        } else {
+            return createOldSoundPool();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    protected SoundPool createNewSoundPool() {
+        AudioAttributes attributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+
+        return new SoundPool.Builder()
+                .setAudioAttributes(attributes)
+                .build();
+    }
+
+    @SuppressWarnings("deprecation")
+    protected SoundPool createOldSoundPool() {
+        return new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
     }
 }
