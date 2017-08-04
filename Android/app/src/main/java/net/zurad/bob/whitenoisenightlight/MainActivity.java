36f57c2af9f2a4ba0429da267274b769f98a58dc
Bob Zurad <bob@zurad.net>
2017-08-04 16:01:51 -0500

    flashlight toggle, but camera causing crash

--- Android/app/src/main/java/net/zurad/bob/whitenoisenightlight/MainActivity.java
+++ Android/app/src/main/java/net/zurad/bob/whitenoisenightlight/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.hardware.Camera;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.net.Uri;
@@ -18,6 +19,8 @@ import android.os.Bundle;
 import android.text.Html;
 import android.text.Spanned;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.WindowManager;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
@@ -26,21 +29,27 @@ import android.widget.Switch;
 import android.media.SoundPool;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback{
 
     LinearLayout _mainLayout;
     SeekBar _seekBar;
     Switch _whiteNoiseSwitch;
+    Switch _flashlightSwitch;
     TextView _brightnessTextView;
     TextView _whiteNoiseTextView;
+    TextView _flashlightTextView;
     SoundPool _soundPool;
     ActionBar _bar;
+    SurfaceView _surfaceView;
+    SurfaceHolder _surfaceHolder;
+    Camera _camera;
+
     int _soundId;
     int _playId;
-
     boolean _canChangeScreenBrightness;
     int _brightnessOnAppStart;
     int _brightnessModeOnAppStart;
+    boolean _hasFlashlight;
 
     final int CODE_WRITE_SETTINGS_PERMISSION = 1001;
 
@@ -53,12 +62,24 @@ public class MainActivity extends AppCompatActivity {
         _mainLayout = (LinearLayout) findViewById(R.id.mainLayout);
         _seekBar = (SeekBar) findViewById(R.id.seekBar);
         _whiteNoiseSwitch = (Switch) findViewById(R.id.whiteNoiseSwitch);
+        _flashlightSwitch = (Switch) findViewById(R.id.flashlightSwitch);
         _brightnessTextView = (TextView) findViewById(R.id.brightnessTextView);
         _whiteNoiseTextView = (TextView) findViewById(R.id.whiteNoiseTextView);
+        _flashlightTextView = (TextView) findViewById(R.id.flashlightTextView);
         _soundPool = createSoundPool();
         _soundId = _soundPool.load(this, R.raw.whitenoise, 1);
         _soundPool.setLoop(_soundId, -1);
         _bar.setTitle(fromHtml("<font color='#000000'>White Noise Night Light</font>"));
+
+        //for camera flash
+        _hasFlashlight = getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        _surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        _surfaceHolder = _surfaceView.getHolder();
+        _surfaceHolder.addCallback(this);
+        _camera = Camera.open();
+        try {
+            _camera.setPreviewDisplay(_surfaceHolder);
+        } catch (java.io.IOException ex) {}
 
         //save starting brightness values
         try {
@@ -104,10 +125,12 @@ public class MainActivity extends AppCompatActivity {
                         _bar.setTitle(fromHtml("<font color='#C0C0C0'>White Noise Night Light</font>"));
                         _brightnessTextView.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.textColorLight));
                         _whiteNoiseTextView.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.textColorLight));
+                        _flashlightTextView.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.textColorLight));
                     } else {
                         _bar.setTitle(fromHtml("<font color='#000000'>White Noise Night Light</font>"));
                         _brightnessTextView.setTextColor(Color.BLACK);
                         _whiteNoiseTextView.setTextColor(Color.BLACK);
+                        _flashlightTextView.setTextColor(Color.BLACK);
                     }
                 }
                 //change color of background
@@ -139,6 +162,25 @@ public class MainActivity extends AppCompatActivity {
                     }
                 } else {
                     _soundPool.pause(_playId);
+                }
+            }
+        });
+
+        //logic for flashlight switch
+        _flashlightSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isOn) {
+                if (isOn) {
+                    Camera.Parameters params = _camera.getParameters();
+                    params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                    _camera.setParameters(params);
+                    _camera.startPreview();
+                } else {
+                    Camera.Parameters params = _camera.getParameters();
+                    params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                    _camera.setParameters(params);
+                    _camera.stopPreview();
+                    _camera.release();
                 }
             }
         });
@@ -243,4 +285,20 @@ public class MainActivity extends AppCompatActivity {
         }
         return result;
     }
+
+    //region SurfaceHolder.Callback implementation
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        _surfaceHolder = holder;
+        try {
+            _camera.setPreviewDisplay(_surfaceHolder);
+        } catch (java.io.IOException e) {}
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        _camera.stopPreview();
+        _surfaceHolder = null;
+    }
+    //endregion
 }
