82342ff5631543641bc3175d67b3e1bc225a3b4d
Bob Zurad <bob@zurad.net>
2017-08-05 14:13:19 -0500

    locked activity to portrait mode

--- Android/app/src/main/java/net/zurad/bob/whitenoisenightlight/MainActivity.java
+++ Android/app/src/main/java/net/zurad/bob/whitenoisenightlight/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -69,6 +70,7 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         _bar = getSupportActionBar();
         _mainLayout = (LinearLayout) findViewById(R.id.mainLayout);
